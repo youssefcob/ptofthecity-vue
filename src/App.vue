e304a95e5477e9e8634725d30ba10041bfa5622c
youssef4140 <youssef.mostafa4140@gmail.com>
2024-05-16 22:34:41 +0300

    configured scss

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -17,10 +17,16 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView /> -->
+  <div></div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+
+div{
+  width:100px;
+  height:100px;
+}
 header {
   line-height: 1.5;
   max-height: 100vh;
