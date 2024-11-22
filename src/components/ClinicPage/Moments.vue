f4c8dec594a5010a1ab7d0b3c0b4b7390f70b87c
youssefcob <164803162+youssefcob@users.noreply.github.com>
2024-11-22 21:36:15 +0200

    clinic moments

---
+++ src/components/ClinicPage/Moments.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { Media } from '@/interfaces/content';
+
+
+
+const props = defineProps({
+    moment: Object as () => Media,
+});
+</script>
+
+<template>
+        <div class="singleMoment" :style="`background-image:url(${props.moment?.path})`"></div>
+</template>
+
+
+<style scoped lang='scss'>
+
+.singleMoment{
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    border-radius: 1rem;
+    margin-bottom: 1rem;
+    cursor: pointer;
+    transition: transform 0.3s;
+    &:hover{
+        transform: scale(1.05);
+    }
+}
+</style>
