a290150e83e27e9b05342116fb2419ee58bcdbab
youssefcob <164803162+youssefcob@users.noreply.github.com>
2024-11-22 21:36:31 +0200

    updated clinics .vue

--- src/components/ClinicPage/Clinic.vue
+++ src/components/ClinicPage/Clinic.vue
@@ -8,6 +8,7 @@ import SingleService from '../ServicesPage/SingleService.vue';
 import Testimonials from './Testimonials/Testimonials.vue';
 import { clinics, clinicNames } from '../HomePage/clinicsSection/Clinics';
 import ClinicsMap from '../sharedComponents/Map.vue';
+import Moments from './Moments.vue';
 const center: Ref<{ lat: number, lng: number }> = ref({ lat: 0, lng: 0 })
 
 const google_api_key = import.meta.env.VITE_GOOGLE_API_KEY as string;
@@ -77,9 +78,9 @@ const getWorkingHours = () => {
             let hours = schedule[day as keyof typeof schedule];
             let dayAcr = capitalizeFirstLetter(day.slice(0, 3));
             if (hours.off) {
-                workingHours += `<span style="font-weight: bold;">${dayAcr}</span>: Off <br>`;
+                workingHours += `<span ><strong>${dayAcr}</strong>: Off </span>`;
             } else {
-                workingHours += `<span style="font-weight: bold;">${dayAcr}</span>: ${convertToAmPm(hours.start)} - ${convertToAmPm(hours.end)} <br>`;
+                workingHours += `<span ><strong>${dayAcr}</strong>: ${convertToAmPm(hours.start)} - ${convertToAmPm(hours.end)} </span>`;
             }
         });
     }
@@ -90,8 +91,19 @@ const renderClinic = async () => {
     await getClinicReviews();
 }
 
+const momentsCarousel : Ref<InstanceType<typeof Moments> | null> = ref(null);
+
+const setMomentsInterval = () => {
+    if (momentsCarousel.value) {
+        setInterval(() => {
+            momentsCarousel.value?.scroll(1);
+        }, 5000);
+    }
+}
 onMounted(async () => {
     await renderClinic();
+
+    setMomentsInterval();
 })
 const infowindow = ref(true); // Will be open when mounted
 
@@ -106,10 +118,16 @@ const markerOptions = { position: center, label: 'L', title: 'LADY LIBERTY' }
 
         <div class="clinic-container">
             <div class="desc-wrapper">
-                <h1>{{ clinic?.name }}</h1>
+                <h1 class="desktop">{{ clinic?.name }}</h1>
                 <p>{{ clinic?.summary }}
 
                 </p>
+                <div class="schedule">
+                    <h2 class="smallHeader">Working Hours:</h2>
+                    <div class="hours">
+                        <p v-html="getWorkingHours()"></p>
+                    </div>
+                </div>
                 <div class="btns-wrapper">
                     <router-link :to="`/booking/${clinic?.name}`" class="btn responsive">
                         {{ $translate('book_now') }}
@@ -117,6 +135,21 @@ const markerOptions = { position: center, label: 'L', title: 'LADY LIBERTY' }
                 </div>
             </div>
             <div class="map-info-wrapper">
+                <div class="info mobile">
+                    <h1>{{ clinic?.name }}</h1>
+                    <div class="address-phone-wrapper">
+                        <a target="_blank" :href="`https://maps.google.com/?q=${clinic?.lat},${clinic?.long}`"
+                            class="ps address">
+                            <strong>Location: </strong>{{ clinic?.street_address }}, {{ clinic?.city }}, {{
+                                clinic?.state }}, {{ clinic?.zip_code
+                            }}
+                        </a>
+                        <p class="ps calls">
+                            <strong> Calls: </strong>{{ clinic?.phone }}
+                        </p>
+                    </div>
+
+                </div>
                 <div class="map">
                     <template v-if="clinic">
                         <ClinicsMap :positions="[clinic]" :zoom="12"
@@ -127,31 +160,37 @@ const markerOptions = { position: center, label: 'L', title: 'LADY LIBERTY' }
 
                     </template>
                 </div>
-                <div class="info">
+                <div class="info desktop">
                     <div class="address-phone-wrapper">
-                        <a target="_blank"  :href="`https://maps.google.com/?q=${clinic?.lat},${clinic?.long}`" class="ps address">
-                            {{ clinic?.street_address }}, {{ clinic?.city }}, {{ clinic?.state }}, {{ clinic?.zip_code
+                        <a target="_blank" :href="`https://maps.google.com/?q=${clinic?.lat},${clinic?.long}`"
+                            class="ps address">
+                            <strong>Location: </strong>{{ clinic?.street_address }}, {{ clinic?.city }}, {{
+                                clinic?.state }}, {{
+                                clinic?.zip_code
                             }}
                         </a>
                         <p class="ps calls">
                             <strong> Calls: </strong>{{ clinic?.phone }}
                         </p>
                     </div>
-                    <div class="schedule">
-                        <p class="ps"><strong>Working Hours:</strong> </p>
-                        <div class="hours">
-                            <p class="ps" v-html="getWorkingHours()"></p>
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </div>
+
         <div class="services">
             <h2 class="sectionHeader-m">
                 {{ clinic?.name }} Clinic Services
             </h2>
             <Carousel class="carousel" v-if="clinic && clinic.services">
                 <SingleService class="singleService" v-for="service in clinic?.services" :service="service" />
+            </Carousel>
+        </div>
+
+        <div class="moments">
+            <h2 class="sectionHeader">Clinic Moments</h2>
+            <Carousel ref="momentsCarousel" class="moment_carousel" v-if="clinic && clinic.media" NoButtons NoIndicator>
+                <Moments class="singleMoment" v-for="moment in clinic?.media" :moment="moment" />
             </Carousel>
         </div>
 
@@ -211,6 +250,18 @@ a {
             flex-direction: column;
             // width:50%;
 
+            .smallHeader {
+                color: $navy;
+            }
+
+            .hours {
+                >p {
+                    display: grid;
+                    grid-template-rows: repeat(4, 1fr);
+                    grid-auto-flow: column;
+                }
+            }
+
             p {
                 margin-top: 1rem;
                 // background-color: red;
@@ -227,6 +278,10 @@ a {
         .map-info-wrapper {
             min-height: 40vh;
 
+            .info.mobile {
+                display: none;
+            }
+
             >.map {
                 height: 30vh;
                 min-height: 40vh;
@@ -238,6 +293,12 @@ a {
                 display: flex;
                 gap: 1rem;
                 flex-wrap: wrap;
+
+                .address-phone-wrapper {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 1rem;
+                }
 
                 .ps {
                     margin: 0;
@@ -340,6 +401,39 @@ a {
 
         }
 
+    }
+}
+
+.moments{
+    @include pagePadding;
+
+    .moment_carousel{
+        height:40rem;
+    }
+    .singleMoment {
+        @include carouselItem2(1);
+
+ 
+
+    }
+}
+
+@media screen and (max-width: 950px) {
+    .container .clinic-container {
+        display: flex;
+        flex-direction: column-reverse;
+
+        .desktop,
+        .info.desktop {
+            display: none;
+        }
+
+        .map-info-wrapper {
+            .info.mobile {
+                display: flex;
+
+            }
+        }
     }
 }
 </style>
